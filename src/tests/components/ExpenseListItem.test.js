b62e8e5063f73cbe16dd84fe71f62da8cd849038
Adam Stringfellow <adam.stringfellow@gmail.com>
2018-02-06 00:17:26 +0000

    setup formatting for amount and createdAt

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import React from 'react';
+import { shallow } from 'enzyme';
 
-import ExpenseListItem from '../../components/ExpenseListItem'
-import expenses from '../fixtures/expenses'
+import ExpenseListItem from '../../components/ExpenseListItem';
+import expenses from '../fixtures/expenses';
 
 test('should render ExpenseListItem correctly', () => {
-  const wrapper = shallow(<ExpenseListItem key={expenses[1].id} {...expenses[1]}/>)
-  expect(wrapper).toMatchSnapshot()
-})
+  const wrapper = shallow(<ExpenseListItem key={expenses[1].id} {...expenses[1]} />);
+  expect(wrapper).toMatchSnapshot();
+});
